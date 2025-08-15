e8f5b4127a5cc23ae453b49b966ddfc384a64348
aspadhye <126921139+aspadhye@users.noreply.github.com>
2025-08-15 10:10:18 -0700

    Assignment added a comment

--- github-api-integration-module.sh
+++ github-api-integration-module.sh
@@ -5,6 +5,7 @@
 #
 #
 #
+# Assignment from Udemy Course: "Ultimate DevOps and Cloud Interview Guide"
 # This script will help users to communicate and retrieve information from GitHub
 # Usage:
 #   Please provide your github token and rest api to the script as input
